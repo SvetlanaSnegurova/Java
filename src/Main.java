13107ba1898ffbf1cdd0e0e44a7bb3b3c19c155f
Svetlana Lazareva <slazareva.sl@gmail.com>
2023-01-21 15:01:48 +0300

    java

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int amout = 15000;
+        int y = 20;
+        int bonus = amout / y;
+
+        System.out.println("Итоговый бонус: " + bonus);
+
+    }
+}
